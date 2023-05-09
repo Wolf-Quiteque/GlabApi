2cf2ee1a76e32fa915db8ebfd17aa2cb964e3168
Wolf-Quiteque <marcioqui3@gmail.com>
2023-05-09 09:10:31 +0100

    gogo

--- routes/cumbigroup/index.js
+++ routes/cumbigroup/index.js
@@ -5,8 +5,18 @@ router.post("/", async (req, res) => {
   try {
     const cliente = await clientPromise;
     const db = cliente.db("glab_admin");
-    const data = await db.collection("cumbigroup").insertOne(req.body);
-    res.status(200).json("sucesso");
+
+    const data1 = await db
+      .collection("cumbigroup")
+      .findOne({ email: req.body.email });
+
+    if (data1) {
+      console.log("email ja existe");
+    } else {
+      const data = await db.collection("cumbigroup").insertOne(req.body);
+    }
+
+    return res.status(200).json("sucesso");
   } catch (err) {
     res.status(500).json(err);
   }
