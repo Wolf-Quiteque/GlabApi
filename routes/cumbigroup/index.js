55feb08c3e49439a589060c586b67d36805a3e5f
Marcio Ragy Daveiga Quiteque <33323596+Wolf-Quiteque@users.noreply.github.com>
2024-10-14 11:19:52 +0000

    lets go

--- routes/cumbigroup/index.js
+++ routes/cumbigroup/index.js
@@ -25,6 +25,31 @@ router.get("/pesquisas", async (req, res) => {
    } catch (err) {
      res.status(500).json(err);
    }
+ });
+
+ router.get("/comprovativos", async (req, res) => {
+  try {
+ 
+     const cliente = await clientPromise;
+     const db = cliente.db("destinos");
+     const resul = await db.collection("comprovativos").find(req.body).toArray();
+ 
+     return res.status(200).json(resul);
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ }); 
+
+ router.post("/add", async (req, res) => {
+  try {
+ 
+     const cliente = await clientPromise;
+     const db = cliente.db("destinos");
+     const resul = await db.collection("comprovativos").insertOne(req.body);
+     return res.status(200).json(resul);
+   } catch (err) {
+     res.status(500).json(err);
+   }
  }); 
 
 module.exports = router;
